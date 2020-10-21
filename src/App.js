07d593150166324a21b5e12b9e245c45c7cd4b56
Hamilton <hamiltonmb.silva@gmail.com>
2020-10-20 21:59:12 -0300

    Criação do AppStyled

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { useSelector } from 'react-redux';
 
+import { Container, Menu, PageBody } from './AppStyled';
+
 import HomeScreen from './pages/HomeScreen';
 import Tela2Screen from './pages/Tela2Screen';
 
@@ -10,16 +12,21 @@ export default () => {
 
     return (
         <BrowserRouter>
-            <h1>Oi, {name}</h1>
+            <Container>
+                <Menu>
 
-            <Switch>
-                <Route exact path="/">
-                    <HomeScreen />
-                </Route>
-                <Route path="/tela2/:nome">
-                    <Tela2Screen />
-                </Route>
-            </Switch>
+                </Menu>
+                <PageBody>
+                    <Switch>
+                        <Route exact path="/">
+                            <HomeScreen />
+                        </Route>
+                        <Route path="/tela2/:nome">
+                            <Tela2Screen />
+                        </Route>
+                    </Switch>
+                </PageBody>                
+            </Container>
         </BrowserRouter>
     );
 }
---
+++ src/AppStyled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+display: flex;
+backgroud-color: #FF0000;
+`;
+
+export const Menu = styled.div``;
+
+export const PageBody = styled.div``;
