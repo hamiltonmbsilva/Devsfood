4c84015eaafb77279f8c7ecb0e234c93c4086de5
Hamilton <hamiltonmb.silva@gmail.com>
2020-10-23 21:49:07 -0300

    Configurando o css do cart

---
+++ src/components/Cart/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { 
+    CartArea,
+    CartHeader,
+    CartIcon,
+    CartText,
+    CartBody
+} from './styled';
+
+export default () => {
+    return (
+        <CartArea>
+            <CartHeader>
+                <CartIcon src="/assets/cart.png" />
+                <CartText>Meu Carrinho (x)</CartText>
+
+            </CartHeader>
+            <CartBody>
+            
+            </CartBody>
+        </CartArea>
+    );
+}
