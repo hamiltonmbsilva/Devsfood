4722309cf748d00726029b442d71452fcd96e588
Hamilton <hamiltonmb.silva@gmail.com>
2020-10-24 21:10:20 -0300

    Criação do component do header, e criando o campo de pesquisa

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Logo, SearchInput } from './styled';
+
+export default () => {  
+
+    return (
+        <Container>
+            <Logo src="/assets/logo.png" />
+            <SearchInput type="text" placeholder="Digite um produto..." />
+        </Container>
+    );
+}
