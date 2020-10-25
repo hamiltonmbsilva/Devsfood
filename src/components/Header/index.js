14f1295f2b4e43eebeea991a336881c7da8a96ea
Hamilton <hamiltonmb.silva@gmail.com>
2020-10-24 21:27:38 -0300

    Configuração para qunado clicar no icone de pesquisa, abrir

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Logo, SearchInput } from './styled';
 
-export default () => {  
+export default () => {
+    
+    const [inputActive, setInputActive] = useState(false);
+
+    const handleInputFocus = () => {
+        setInputActive(true);
+    }
+
+    const handleInputBlur = () => {
+        setInputActive(false);
+    }
 
     return (
         <Container>
             <Logo src="/assets/logo.png" />
-            <SearchInput type="text" placeholder="Digite um produto..." />
+            <SearchInput 
+                type="text" 
+                placeholder="Digite um produto..." 
+                active={inputActive}
+                onFocus={handleInputFocus}
+                onBlur={handleInputBlur}
+                
+            />
         </Container>
     );
 }
