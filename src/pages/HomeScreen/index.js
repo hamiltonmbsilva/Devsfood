d2b807275336cad4aa349b627c521b5c37a53739
Hamilton <hamiltonmb.silva@gmail.com>
2020-10-26 21:10:17 -0300

    Configurando a api no front end pegando as categorias

--- src/pages/HomeScreen/index.js
+++ src/pages/HomeScreen/index.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from "react-router-dom";
 import { Container } from './styled';
+
+import api from '../../api';
 
 import Header from '../../components/Header';
 
@@ -8,6 +10,13 @@ export default () => {
     const history = useHistory();
     const [headerSearch, setHeaderSearch] = useState('');
     
+    useEffect(()=>{
+        const getCategories = async () => {
+            const categories = await api.getCategories();
+            console.log(categories);
+        };
+        getCategories();
+    }, []);
 
     return (
         <Container>
