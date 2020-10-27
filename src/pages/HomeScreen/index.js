cfb621f530cdfe58dea1067afea914b0a96d1d77
Hamilton <hamiltonmb.silva@gmail.com>
2020-10-26 21:38:48 -0300

    Criando os component de categoria item

--- src/pages/HomeScreen/index.js
+++ src/pages/HomeScreen/index.js
@@ -1,26 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from "react-router-dom";
-import { Container } from './styled';
+import { Container, CategoryArea, CategoryList } from './styled';
 
 import api from '../../api';
 
 import Header from '../../components/Header';
+import CategoryItem from '../../components/CategoryItem';
 
 export default () => {
     const history = useHistory();
     const [headerSearch, setHeaderSearch] = useState('');
+    const [categories, setCategories] = useState([]);
     
     useEffect(()=>{
         const getCategories = async () => {
-            const categories = await api.getCategories();
-            console.log(categories);
+            const cat = await api.getCategories();
+
+            if( cat.error == ''){
+                setCategories(cat.result);
+            }
         };
+
         getCategories();
     }, []);
 
     return (
         <Container>
             <Header search={headerSearch} onSearch={setHeaderSearch} />
+
+            {categories.length > 0 && 
+                <CategoryArea>
+                    Selecione uma categoria
+                    <CategoryList>
+                        <CategoryItem title="Todas as categorias" image="food-and-restaurant.png" />  
+                    </CategoryList>
+                </CategoryArea>
+            }
         </Container>
     );
 }
