3f52649c029e936aa7702a95e1ae1d3ab0abdc53
Hamilton <hamiltonmb.silva@gmail.com>
2020-11-07 09:55:25 -0300

    Configurando os productos para ser exibidos

--- src/components/ProductItem/index.js
+++ src/components/ProductItem/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
 
-import { Container } from './styled';
+import {
+    Container,
+    ProductPhotoArea,
+    ProductPhoto,
+    ProductInfoArea,
+    ProductName,
+    ProductPrice,
+    ProductIngredients,
+    ProductButtonArea,
+    ProductButton
 
-export default () =>{
+} from './styled';
+
+export default ({data}) =>{
     return (
         <Container>
-        
+
+            <ProductPhotoArea>
+                <ProductPhoto src={data.image} />
+            </ProductPhotoArea>
+
+            <ProductInfoArea>
+                <ProductName>{data.name}</ProductName>
+                <ProductPrice>{data.price}</ProductPrice>
+                <ProductIngredients>{data.ingredients}</ProductIngredients>
+            </ProductInfoArea>
+
+            <ProductButtonArea>
+                <ProductButton src="/assets/next.png" />
+            </ProductButtonArea>        
         </Container>
     )
 }
