170ad4a870aee820242db8e92c7ec811c439b4e4
Hamilton <hamiltonmb.silva@gmail.com>
2022-02-07 15:35:42 -0300

    Configurando o modal dp produco colocando as informações no local correto

--- src/components/ModalProduct/styled.js
+++ src/components/ModalProduct/styled.js
@@ -11,28 +11,49 @@ export const ProductArea = styled.div`
     display: flex;
 `;
 export const ProductButtons = styled.div`
-    height: 50px;
-    background-color:#00FF00;    
+   margin-top: 10px;
+   display: flex;
+   justify-content: flex-end;
 `;
 
 export const ProductPhoto = styled.img`
     width: 310px;
+    border-radius: 10px;
 `;
 export const ProductInfoArea = styled.div`
    flex:1;
    display: flex;
    flex-direction: column;
    justify-content: space-between;
+   margin-left: 10px;
 `;
 
 
 export const ProductDetails = styled.div`
-    height: 50px;    
-    background-color:#FF0000;
+    
 `;
 export const ProductQuantityArea = styled.div`
-height: 50px;    
-background-color:#0000FF;
+    height: 50px;    
+    background-color:#0000FF;
 `;
 
+export const ProductName = styled.div`
+    font-size: 30px;
+    font-wight: bold;
+`;
 
+export const ProductIngredients = styled.div`
+    font-size: 14px;
+`;
+
+export const ProductButton = styled.button`
+    border: 0;
+    background-color:#073C07;
+    box-shadow:4px 5px 0px #999;
+    color: #FFF;
+    font-size: 22px;
+    font-weight: bold;
+    padding: 10px 20px;
+    margin-left: 10px;
+    border-radius: 5px;
+`;
