6624b899617de7945a37a5f8493dc3475b880fd3
Hamilton <hamiltonmb.silva@gmail.com>
2022-02-09 23:19:42 -0300

    Add files via upload

---
+++ build/precache-manifest.2a018a900b13ab4ec69e065f8aee5c5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5adb56f9ab0e88bea1cce9632a84a183",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d43e7cfe1414389da3c5",
+    "url": "/static/css/main.edb8f0a1.chunk.css"
+  },
+  {
+    "revision": "f911c61c81e0e0dcfaa0",
+    "url": "/static/js/2.5c9b5a53.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5c9b5a53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d43e7cfe1414389da3c5",
+    "url": "/static/js/main.925d3301.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  }
+]);
